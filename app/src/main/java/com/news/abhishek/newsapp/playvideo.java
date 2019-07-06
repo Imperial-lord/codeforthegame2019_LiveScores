03b5971052b179442a687982f7ffcc0b4c504943
Anjali Soni <51020896+anjalisoni3655@users.noreply.github.com>
2019-07-06 20:26:58 +0530

    Update playvideo.java

--- app/src/main/java/com/news/abhishek/newsapp/playvideo.java
+++ app/src/main/java/com/news/abhishek/newsapp/playvideo.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.VideoView;
 import android.widget.MediaController;
 
@@ -11,14 +13,28 @@ import com.example.android.cricket.R;
 
 public class playvideo extends AppCompatActivity {
 
+    ImageButton imageButton;
+    VideoView videoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playvideo);
-        VideoView videoView = findViewById(R.id.video_view);
+
+         videoView=findViewById(R.id.video_view);
+         imageButton=findViewById(R.id.play_button);
+
+    }
+
+    public void playvid(View view)
+    {
+         videoView = findViewById(R.id.video_view);
         String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.video;
         Uri uri = Uri.parse(videoPath);
         videoView.setVideoURI(uri);
+        videoView.start();
+
+        imageButton.setVisibility(View.INVISIBLE);
+
 
         MediaController mediaController = new MediaController(this);
         videoView.setMediaController(mediaController);
